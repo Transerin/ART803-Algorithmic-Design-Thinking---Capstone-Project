981a19cd763d579f78e8eb8ff2d2a4e6e56f2d91
Transerin <117934024+Transerin@users.noreply.github.com>
2023-05-18 15:01:59 +0100

    Update 1_Building_Design.py

--- pages/1_Building_Design.py
+++ pages/1_Building_Design.py
@@ -30,13 +30,14 @@ def read_json(model_file_path, color: str):
     topology_data = [Plotly.DataByTopology(topology=topologies[i], showEdges=False, showVertices=False, faceLabelKey='Type Name', faceOpacity=1, faceColor=color) for i in range(len(topologies))]
     return sum(topology_data, [])
 
-solid_wall_file_path = Path('./3D models/Dynamo for Streamlit/Merged Solid Wall Topological Model.json')
-partition_wall_file_path = Path('./3D models/Dynamo for Streamlit/Merged Partition Wall Topological Model.json')
-roof_file_path = Path('./3D models/Dynamo for Streamlit/Merged Roof Topological Model.json')
-floor_file_path = Path('./3D models/Dynamo for Streamlit/Merged Floor Topological Model.json')
+#solid_wall_file_path = Path('./3D models/Dynamo for Streamlit/Merged Solid Wall Topological Model.json')
+#partition_wall_file_path = Path('./3D models/Dynamo for Streamlit/Merged Partition Wall Topological Model.json')
+#roof_file_path = Path('./3D models/Dynamo for Streamlit/Merged Roof Topological Model.json')
+#floor_file_path = Path('./3D models/Dynamo for Streamlit/Merged Floor Topological Model.json')
 column_file_path = Path('./3D models/Dynamo for Streamlit/Merged Column Topological Model.json')
 
-merged_data = read_json(solid_wall_file_path, color='rgb(132, 133, 135)') + read_json(partition_wall_file_path, color='rgb(245, 245, 245)') + read_json(roof_file_path, color='rgb(245, 245, 245)') + read_json(floor_file_path, color='rgb(245, 245, 245)') + read_json(column_file_path, color='rgb(245, 245, 245)')
+#merged_data = read_json(solid_wall_file_path, color='rgb(132, 133, 135)') + read_json(partition_wall_file_path, color='rgb(245, 245, 245)') + read_json(roof_file_path, color='rgb(245, 245, 245)') + read_json(floor_file_path, color='rgb(245, 245, 245)') + read_json(column_file_path, color='rgb(245, 245, 245)')
+merged_data = read_json(column_file_path, color='rgb(245, 245, 245)')
 topology_fig = Plotly.FigureByData(data=merged_data, height=800)
 
 # ----------------------------------------------------------------- Part 1 3D Viewer -----------------------------------------------------------------
