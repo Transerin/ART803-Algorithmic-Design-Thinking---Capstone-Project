16f65a848d7c4a8a4270d50555e409a79f3626a6
Transerin <117934024+Transerin@users.noreply.github.com>
2023-05-18 17:50:50 +0100

    Update 1_Building_Design.py

--- pages/1_Building_Design.py
+++ pages/1_Building_Design.py
@@ -25,15 +25,27 @@ st.title('Building Design')
 st.markdown(body='This page could take a while for loading, please be patient.')
 
 # ----------------------------------------------------------------- Part 0 Prepare Topological figure data -----------------------------------------------------------------
+def read_json(model_file_path, color: str):
+    topologies = Topology.ByJSONPath(model_file_path)
+    topology_data = [Plotly.DataByTopology(topology=topology, showEdges=False, showVertices=False, faceLabelKey='Type Name', faceOpacity=1, faceColor=color) for topology in topologies]
+    return sum(topology_data, [])
 
+solid_wall_file_path = Path('./3D models/Dynamo for Streamlit/Merged Solid Wall Topological Model.json')
+partition_wall_file_path = Path('./3D models/Dynamo for Streamlit/Merged Partition Wall Topological Model.json')
+roof_file_path = Path('./3D models/Dynamo for Streamlit/Merged Roof Topological Model.json')
+floor_file_path = Path('./3D models/Dynamo for Streamlit/Merged Floor Topological Model.json')
+column_file_path = Path('./3D models/Dynamo for Streamlit/Merged Column Topological Model.json')
+
+merged_data = read_json(solid_wall_file_path, color='rgb(132, 133, 135)') + read_json(partition_wall_file_path, color='rgb(245, 245, 245)') + read_json(roof_file_path, color='rgb(245, 245, 245)') + read_json(floor_file_path, color='rgb(245, 245, 245)') + read_json(column_file_path, color='rgb(245, 245, 245)')
+
+topology_fig = Plotly.FigureByData(data=merged_data, height=800)
 # ----------------------------------------------------------------- Part 1 3D Viewer -----------------------------------------------------------------
 st.header(f'3D Viewer')
 
 col1, col2 = st.columns(2)
 with col1:
     # 3D main structure model viewer
-    #st.plotly_chart(topology_fig, use_container_width=True)
-    components.iframe(src="https://cloud.viktor.ai/public/test-6ed", height=800)
+    st.plotly_chart(topology_fig, use_container_width=True)
     st.markdown(body='This is a Topological model viewer which shows the information of main structures. Please use your cursor to hover over the components to explore.')
 
 with col2:
