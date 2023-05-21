becfd34bad1b5d8d5f24e6c3362f8470f7ef1b7d
Transerin <117934024+Transerin@users.noreply.github.com>
2023-05-21 12:34:37 +0100

    Update 2_Daylight_Analysis.py

--- pages/2_Daylight_Analysis.py
+++ pages/2_Daylight_Analysis.py
@@ -107,18 +107,20 @@ with st.expander(label='Control Panel', expanded=True):
     col1, col2 = st.columns(2)
     with col1:
         st.selectbox('Global Colorsets Selector', colorsets.keys(), key='colorset_select', on_change=handle_colorset, help="If you don't like the predefined colorset, you can use this selector to change the colorset.")
+        
+        col3, col4, col5, col6 = st.columns(4)
+        with col3:
+            st.button('Reset camera', key='reset_camera', on_click=handle_resetcamera)
+        with col4:
+            st.button('Screenshot', key='streamlit-screenshot', on_click=handle_screenshot)
+        with col5:
+            st.checkbox('Toggle Orthographic / Perspective', value=False, key='toggle_ortho', on_change=handle_toggleortho)
+        with col6:
+            st.checkbox('Sidebar', value=False, key='sidebar_toggle', help='Show/Hide the side toolbar.')
+    
     with col2:
         st.selectbox('Select an Orthogonal View', views, key='views', index=0, on_change=handle_views)
-    
-    col3, col4, col5, col6 = st.columns(4)
-    with col3:
-        st.button('Reset camera', key='reset_camera', on_click=handle_resetcamera)
-    with col4:
-        st.button('Screenshot', key='streamlit-screenshot', on_click=handle_screenshot)
-    with col5:
-        st.checkbox('Toggle Orthographic / Perspective', value=False, key='toggle_ortho', on_change=handle_toggleortho)
-    with col6:
-        st.checkbox('Sidebar', value=False, key='sidebar_toggle', help='Show/Hide the side toolbar.')
+
 
 if analysis_type == 'Daylight Factor Analysis':
     viewer(key='daylight_factor', content=daylight_factor_vtkjs, sidebar=st.session_state.sidebar_toggle, action_stack=st.session_state.action_stack)
@@ -129,6 +131,8 @@ if analysis_type == 'Daylight Factor Analysis':
     st.header('Terminology')
     # https://docs.ladybug.tools/hb-radiance-primer/components/3_recipes/daylight_factor
     st.markdown('Daylight Factor (DF) is defined as the ratio of the indoor daylight illuminance to outdoor illuminance under an unobstructed overcast sky. It is expressed as a percentage between 0 and 100.')
+    
+    st.session_state.action_stack.clear()
 elif analysis_type == 'Annual Daylight Analysis':
     viewer(key='annual_dalight', content=annual_daylight_vtkjs, sidebar=st.session_state.sidebar_toggle, action_stack=st.session_state.action_stack)
 
@@ -138,5 +142,5 @@ elif analysis_type == 'Annual Daylight Analysis':
     st.header('Terminology')
     # https://docs.ladybug.tools/hb-radiance-primer/components/3_recipes/annual_daylight
     st.markdown('This recipe uses an enhanced 2-phase method for daylight simulation which accurately models direct sun by tracing rays from each sensor to the solar position at each hour of the calculation. The resulting illuminance is used to compute the following metrics: **Daylight Autonomy (DA)** - The percentage of occupied hours that each sensor recieves more than the illuminance threshold. **Continuous Daylight Autonomy (cDA)** - Similar to DA except that values below the illuminance threshold can still count partially towards the final percentage. **Useful Daylight Illuminance (UDI)** - The percentage of occupied hours that illuminace falls between minimum and maximum thresholds')
-
-st.session_state.action_stack.clear()
+    
+    st.session_state.action_stack.clear()
